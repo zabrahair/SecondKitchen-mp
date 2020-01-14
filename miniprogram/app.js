700cd91789509d4067be8ff73b89bb621d2d53f9
Dean Wang <zabrahair@gmail.com>
2020-01-14 16:01:36 +0800

    1. 添加自动升级的代码。

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -8,6 +8,33 @@ const utils = require('./utils/util.js');
 const USER_ROLE = require('./const/userRole.js')
 const userApi = require('./api/user');
 
+const updateManager = wx.getUpdateManager()
+/**
+ * 小程序升级检测
+ */
+updateManager.onCheckForUpdate(function (res) {
+  // 请求完新版本信息的回调
+  debugLog("onCheckForUpdate",res.hasUpdate)
+})
+
+updateManager.onUpdateReady(function () {
+  wx.showModal({
+    title: '更新提示',
+    content: '新版本已经准备好，是否重启应用？',
+    success: function (res) {
+      if (res.confirm) {
+        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+        updateManager.applyUpdate()
+      }
+    }
+  })
+})
+
+updateManager.onUpdateFailed(function () {
+  // 新版本下载失败
+})
+
+
 App({
   onLaunch: function () {
     
